0cc1706b3cab261faa62539a8114640b3ab970fb
easychessanimations <chessbotroulette@gmail.com>
2020-05-11 05:40:55 +0200

    Pretty print bitboard

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -4,6 +4,27 @@ pub trait Bitboard {
 
 impl Bitboard for u64 {
     fn pretty_print_string(&self) -> String {
-        format! {"{:#016x}", &self}
+        let mut bb = *self;
+        let mut buff = "".to_string();
+        let mut bits = 0;
+        loop {
+            if bits % 8 == 0 {
+                buff += "*"
+            }
+            if bb & 1 == 1 {
+                buff += "1"
+            } else {
+                buff += "0"
+            }
+            if bits % 8 == 7 {
+                buff += "*\n"
+            }
+            bb = bb >> 1;
+            bits = bits + 1;
+            if bits == 64 {
+                break;
+            }
+        }
+        format! {"bitboard {:#016x}\n**********\n{}**********\n", &self, buff}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rustenginelib::bitboard::Bitboard;
 
 fn main() {
-    let x: u64 = 232323;
+    let x: u64 = 0xffff00000000ffff;
 
     println!("{}", x.pretty_print_string())
 }
