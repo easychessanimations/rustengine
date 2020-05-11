c9a5008285755708252dc5b9530e4ec637cdc8ea
easychessanimations <chessbotroulette@gmail.com>
2020-05-11 13:33:00 +0200

    Typo

--- gen.js
+++ gen.js
@@ -13,7 +13,7 @@ let squareBuff = []
 for(let rank = 0; rank < NUM_RANKS; rank++){
 	rankBuff.push(`/// RANK_${RANK_NAMES[rank]} represents rank '${(rank+1)}' of a chess board\npub const RANK_${RANK_NAMES[rank]} : Rank = ${rank};`)
 	for(let file = 0; file < NUM_FILES; file++){
-		squareBuff.push(`/// SQUARE_${FILE_NAMES[file].toUpperCase()}${RANK_NAMES[rank]} represents rank square '${FILE_NAMES[file]}${RANK_NAMES[rank]}' of a chess board\npub const SQUARE_${FILE_NAMES[file].toUpperCase()}${RANK_NAMES[rank]}: Square = ${rank*NUM_FILES+file};`)
+		squareBuff.push(`/// SQUARE_${FILE_NAMES[file].toUpperCase()}${RANK_NAMES[rank]} represents square '${FILE_NAMES[file]}${RANK_NAMES[rank]}' of a chess board\npub const SQUARE_${FILE_NAMES[file].toUpperCase()}${RANK_NAMES[rank]}: Square = ${rank*NUM_FILES+file};`)
 		if(rank == 0){
 			fileBuff.push(`/// FILE_${FILE_NAMES[file].toUpperCase()} represents file '${FILE_NAMES[file]}' of a chess board\npub const FILE_${FILE_NAMES[file].toUpperCase()} : File = ${file};`)
 		}		
